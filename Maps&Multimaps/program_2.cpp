2eecdb15af4bd32b7c890529e80de1921f50e62f
Bhavya <bhavyaaggarwal278@gmail.com>
2020-10-15 03:49:53 +0530

    maps&multimaps

---
+++ Maps&Multimaps/program_2.cpp
@@ -0,0 +1,41 @@
+// in multimap we are able to insert multiple different value of a same key like-:
+// a->apple
+// a->arvind
+
+// Time complexity of insert(),find(),erase() is log(n) becoz of self balancing BST
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+int main()
+{
+    multimap<char,string> m;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        char ch;
+        string s;
+        cin>>ch>>s;
+        m.insert(make_pair(ch,s));
+    }
+    auto it=m.begin();
+    m.erase(it);
+
+    auto it2=m.lower_bound('b');
+    auto it3=m.upper_bound('d');
+    for(auto it4=it2;it4!=it3;it4++){
+        cout<<it4->second<<" ,"<<endl;
+    }
+    // Search for cat
+    auto f=m.find('c');
+    if(f->second=="cat"){
+        m.erase(f);
+    }
+    //Try to print the entire map
+    for(auto p:m)
+    {
+        cout<<p.first<<" -> "<<p.second<<endl;
+    }
+
+    return 0;
+}
